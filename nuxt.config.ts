e4af9b80b21c394828a59efc56ccd57063639d1c
Krisztian Olah <krisztian.olah@genesys.com>
2024-03-08 00:07:47 +0100

    add tw and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: process.env.PRODUCTION !== 'true'},
-})
+  devtools: { enabled: process.env.PRODUCTION !== "true" },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  css: ["@/assets/css/tailwind.css"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
